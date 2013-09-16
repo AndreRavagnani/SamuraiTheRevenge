eeccc754a337a73c654a8b3196a38b2cafad6e1a
AndreRavagnani <andreskiter@gmail.com>
2013-09-16 20:11:32 -0300

    Movimentação Personagem
    Alterado para o personagem andar até o inimigo

--- main.lua
+++ main.lua
@@ -1,7 +1,5 @@
 local sprite = require "sprite"
 
-
-
 local background = display.newImage("ground.png",true)
 background.x = display.contentWidth /2
 background.y = display.contentHeight / 2
@@ -9,11 +7,11 @@ background.y = display.contentHeight / 2
 local score = 0 
 local pontuacao = display.newText(score, display.contentWidth, 0, native.systemFontBold,36)
 pontuacao:setTextColor(255,255,255)
---[[
+
 local pers = display.newImage("char2.png")
 pers.x = display.contentWidth /2
 pers.y = display.contentHeight /2
-]]
+
 local soco_mp3 = audio.loadSound("soco.mp3")
 local morte_mp3= audio.loadSound("morte.mp3")
 
@@ -28,9 +26,9 @@ local function restartEnemy (event)
 	enemy.y = math.random(enemy.height/2, display.contentHeight - enemy.height)
 	end
 	
-
---transition.to(enemy,{time=1000, x = pers.x, y = pers.y})
 --[[
+transition.to(enemy,{time=1000, x = pers.x, y = pers.y})
+
 while ( enemy.x ~= pers.x or enemy.y ~= pers.y)
 	do
 		if( enemy.x > pers.x) then
@@ -62,22 +60,14 @@ sprite.add(spriteSet2,"punch",1,2,1000,1)
 pers = sprite.newSprite(spriteSet2)
 pers:prepare("punch")
  ]]
- 
- local sheetData = { width=90, height=110, numFrames=50, sheetContentWidth=1024, sheetContentHeight=1024 }
- 
-  local mySheet = graphics.newImageSheet( "streefighter_a2_ryu.png", sheetData )
- 
-  local sequenceData = {
-    { name = "punch", start=1, count=1, time=1000 },
-    { name = "walk", start=1, count=6, time=1000 }
-  }
-  
+
+  --[[
   local pers = display.newSprite( mySheet, sequenceData )
 pers.x = display.contentWidth/2 + 128
 pers.y = display.contentHeight/2
 pers:setSequence( "walk" )
 pers:play()
- 
+ ]]
 local enemy2 = display. newImage("enemy.png")
 enemy2.x =  math.random(enemy2.width/2, display.contentWidth - enemy2.width)
 enemy2.y = math.random(enemy2.height/2, display.contentHeight - enemy2.height)
@@ -94,11 +84,7 @@ local function updateScore(event)
 end
 
 function enemy:tap( event)
-	pers.x = enemy.x -40
-	pers.y = enemy.y 
-	pers:setSequence("punch")
-	pers:play()
-	audio.play(soco_mp3)
+	transition.to(pers,{time=850, x = enemy.x -40, y = enemy.y, onComplete = audio.play(soco_mp3)})
 	enemy.hp = enemy.hp - 1
 	if( enemy.hp <=0) then
 	audio.play(morte_mp3)
@@ -109,8 +95,7 @@ function enemy:tap( event)
 end
 
 function enemy2:tap( event)
-	pers.x = enemy2.x -40
-	pers.y = enemy2.y 
+	transition.to(pers,{time=850, x = enemy2.x -40, y = enemy2.y, onComplete = audio.play(soco_mp3)}) 
 	audio.play(soco_mp3)
 	enemy2.hp = enemy2.hp -1
 	if (enemy2.hp <=0) then
