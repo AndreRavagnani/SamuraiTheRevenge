167e6b9d8e0da0ae2d3a1afc941f2653f301eac2
AndreRavagnani <andreskiter@gmail.com>
2013-11-03 21:04:07 -0300

    Criado Stage Clear e o Novo Enemy Sprite
    Começando a finalização do jogo

--- game.lua
+++ game.lua
@@ -2,7 +2,7 @@ local sprite = require "sprite"
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
 
-local image, enemy, enemy2, pers, pontuacao, hp, life = 3
+local image, enemy, enemy2, pers, pontuacao, hp, life = 3, stageClear
 
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
@@ -16,6 +16,13 @@ image.x = display.contentWidth /2
 image.y = display.contentHeight / 2
 screenGroup:insert( image )
 
+	 stageClear = display.newImage("stageClear.png",true)
+stageClear.x = display.contentWidth /2
+stageClear.y = display.contentHeight / 2
+stageClear.isVisible = false
+screenGroup:insert( stageClear )
+
+
 	hp = display.newImage("NinjaFull.png")
 	hp.x = 0
 	screenGroup:insert( hp) 
@@ -25,6 +32,17 @@ local score = 0
  pontuacao = display.newText(score, display.contentWidth, 0, native.systemFontBold,36)
 pontuacao:setTextColor(255,255,255)
 screenGroup:insert( pontuacao )
+
+local sheetDataEnemy = { width=39, height=119, numFrames=3}
+local sheetEnemy1 = graphics.newImageSheet ("Enemy1Sprite.png", sheetDataEnemy)
+
+local sheetDataEnemy2 = { width=70, height=83, numFrames=5}
+local sheetEnemy2 = graphics.newImageSheet ("Enemy1Down.png", sheetDataEnemy2)
+
+local sequenceData2 = {
+                { name="walk", sheet=sheetEnemy1, start=1, count=3, time=500, loopCount=0 },
+                { name="down", sheet=sheetEnemy2, start=1, count=5, time=500, loopCount=1 }
+                }
 
 
 local sheetData1 = { width=81, height=120, numFrames=3 }
@@ -46,11 +64,12 @@ pers.y = display.contentHeight/2
 pers:play()
 	screenGroup:insert( pers )
 
- enemy = display. newImage("enemy.png")
+ enemy = display.newSprite(sheetEnemy1,sequenceData2)
 enemy.hp =3
 enemy.x =  math.random(enemy.width/2, display.contentWidth - enemy.width)
 enemy.y = math.random(enemy.height/2, display.contentHeight - enemy.height)
 enemy.cd = 5000
+enemy:play()
 	screenGroup:insert( enemy )
 	
 	 enemy2 = display. newImage("enemy.png")
@@ -87,6 +106,12 @@ local function walk()
         pers:play()
 end
 
+local function walkEnemy()
+        enemy:setSequence( "walk" )
+        enemy:play()
+		restartEnemy()
+end
+
 local function updateScore(event)
  pontuacao.text = tonumber(pontuacao.text) + 100
 end
@@ -101,9 +126,11 @@ local morte_mp3= audio.loadSound("morte.mp3")
 	enemy.hp = enemy.hp - 1
 	timer.performWithDelay( 600, walk )
 	if( enemy.hp <=0) then
+	enemy:setSequence("down")
+	enemy:play()
 	audio.play(morte_mp3)
+	timer.performWithDelay(2000,walkEnemy)
 	updateScore()
-	restartEnemy()
 	end
 
 end
@@ -157,9 +184,17 @@ print(timestage)
 timestage = timestage -1
 timestamp.text = timestage
 
+if(timestage <= 0 ) then
+stageClear.isVisible = true;
+stageClear:toFront()
+pers.isVisible = false
+enemy.isVisible = false
+enemy2.isVisible = false
+end
+
+
 end
 timer.performWithDelay(1000,goEnemys,60)
-timer.performWithDelay(5000,goEnemys,60)
 
 
 enemy:addEventListener("tap", enemy)
